a24c8de6960ab5dd43ca9c8cb5a65e1941ae2475
Jonghwan Hyeon <jonghwanhyeon@kaist.ac.kr>
2019-05-06 21:23:26 +0800

    Update iptables

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -33,5 +33,7 @@ iptables -A FORWARD --match policy --pol ipsec --dir in --proto esp -s 10.10.10.
 iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d 10.10.10.0/24 -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -m policy --pol ipsec --dir out -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -j MASQUERADE
+iptables -t mangle -A FORWARD -m policy --pol ipsec --dir in -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+iptables -t mangle -A FORWARD -m policy --pol ipsec --dir out -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 
 ipsec start --nofork
